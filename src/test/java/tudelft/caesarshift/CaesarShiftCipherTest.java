e2248027dc69e9f4a9a3439c11dbb8deb8c22084
colinycw <colinyangcw@hotmail.com>
2019-07-24 13:30:49 +0800

    Complete caesarshift test

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,37 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
 
+    private CaesarShiftCipher caesar;
+
+    @BeforeEach
+    public void initialize() {
+        caesar = new CaesarShiftCipher();
+    }
+
+    @Test
+    public void shiftPositive1() {
+        Assertions.assertEquals("bcd", caesar.CaesarShiftCipher("abc", 1));
+    }
+
+    @Test
+    public void shiftPositive10() {
+        Assertions.assertEquals("klm", caesar.CaesarShiftCipher("abc", 10));
+    }
+
+    @Test
+    public void shiftMinus1() {
+        Assertions.assertEquals("zab", caesar.CaesarShiftCipher("abc", -1));
+    }
+
+    @Test
+    public void shiftMinus5() {
+        Assertions.assertEquals("vwx", caesar.CaesarShiftCipher("abc", -5));
+    }
 }
